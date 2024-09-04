b1fc5654be5d4f48b0550464a91f663063a5695e
Leon <12817042+thedevleon@users.noreply.github.com>
2024-09-04 14:43:57 +0200

    fix: remove wait_until_idle before sleep, it's not needed

--- src/driver.rs
+++ src/driver.rs
@@ -226,12 +226,17 @@ where
         // We can't use send_with_data, because the data function will also wait_until_idle,
         // but after sending the deep sleep command, busy will not be cleared,
         // maybe as a feature to signal the device won't be able to process further instuctions until woken again.
+<<<<<<< HEAD
         self.interface
             .send_commands(DataFormat::U8(&[command::DEEP_SLEEP]))
             .await?;
         self.interface
             .send_data(DataFormat::U8(&[flag::DEEP_SLEEP_MODE_1]))
             .await?;
+=======
+        self.interface.send_commands(DataFormat::U8(&[command::DEEP_SLEEP])).await?;
+        self.interface.send_data(DataFormat::U8(&[flag::DEEP_SLEEP_MODE_1])).await?;
+>>>>>>> 58466a9 (fix: remove wait_until_idle before sleep, it's not needed)
         Ok(())
     }
 
